40f695f9ae51774af78813b632992e07392b8995
Stamatis Theocharous <2380138t@student.gla.ac.uk>
2022-12-09 20:22:00 +0000

    Updated a working version of Windows for Pyterrier

--- Fair-Ranking-LTR/data.py
+++ Fair-Ranking-LTR/data.py
@@ -2,4 +2,8 @@ import pyterrier as pt
 
 
 def download_data(dataset):
-    print('Hello')
+    dataset = pt.get_dataset(dataset)
+    indexer = pt.IterDictIndexer('./indices/trec-fair_2021')
+    index_ref = indexer.index(dataset.get_corpus_iter(), fields=['title', 'text', 'url'])
+    print('Get Data')
+    print(index_ref)
--- Fair-Ranking-LTR/main.py
+++ Fair-Ranking-LTR/main.py
@@ -1,16 +1,19 @@
 import pyterrier as pt
 from data import *
+import os
 
 def main():
     print('Heello')
 
 
 if __name__ == "__main__":
+    os.environ["JAVA_HOME"] = "C:\Program Files\Java\jdk-19"
     if not pt.started:
         pt.init()
-    download_data = False
-    if download_data == False:
-        download_data('irds:trec-fair-2021')
+    print('Pyterrier started')
+    downloaded_data = False
+    if downloaded_data == False:
+        download_data('irds:trec-fair/2021')
     main()
 
 
