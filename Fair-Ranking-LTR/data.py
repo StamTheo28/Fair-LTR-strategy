7103ae659f4f41b270e3055d7c862e6182a9c7a6
Stamatis Theocharous <2380138t@student.gla.ac.uk>
2023-01-21 16:17:12 +0200

    Imported dataset, build index, and relevant data features

--- Fair-Ranking-LTR/data.py
+++ Fair-Ranking-LTR/data.py
@@ -1,30 +1,40 @@
 import pyterrier as pt
 import pandas as pd
+import ir_datasets
+import json
 
 
 def download_data_p():
-    print('Getting Data')
     train_topics_path = "/media/sf_Trec-fair-2022/trec_2022_train_reldocs.json"
     corpus_path = "/media/sf_Trec-fair-2022/trec_corpus_20220301_plain.json/trec_corpus_20220301_plain.json"
     articles_path = "/media/sf_Trec-fair-2022/trec_2022_articles_discrete.json/trec_2022_articles_discrete_V2.json"
+    queries_path = "/media/sf_Trec-fair-2022/trec_2022_train_reldocs.json"
     data = pd.read_json(corpus_path, orient='records', lines = True, chunksize=5)
-    articles = pd.read_json(articles_path, orient='records', lines = True)
+    metadata = pd.read_json(articles_path, orient='records', lines = True, chunksize=5)
     train_topics = pd.read_json(train_topics_path, orient='records', lines = True, chunksize=5)
-    #print(train_topics.columns)
-    count = 0
-    for a in articles:
-        count += len(a)
-    print(count)
-    #print(len(train_topics))
-
-
-
-
-
+    queries = pd.read_json(queries_path, orient='records', lines = True, chunksize=5)
+    print('Data retrieved')
+    return data, metadata, train_topics, queries
+    
+def download(dataset_name, pt):
+    print('downloading')
+    pt.download.trec_fair_2022()
+    print('downloaded')
+    #dataset = pt.get_dataset('irds:trec-fair/2022/train')
+    #print(dataset)
 
 def download_data(dataset_name):
-    dataset = pt.get_dataset(dataset_name)
-    indexer = pt.IterDictIndexer('./indices/trec-fair_2022', threads=8)
-    index_ref = indexer.index(dataset.get_corpus_iter(), fields=['title', 'text', 'url'])
-    print('Get Data')
-    print(index_ref)
+    print('Loading Data')
+    indexer = pt.TRECCollectionIndexer(dataset_name, blocks=True)
+ 
+    #indexer = pt.IterDictIndexer('./indices/trec-fair_2022')
+    
+    # index_ref = indexer.index(dataset, fields=['title', 'text', 'url'])
+    print('Data Successfully loaded')
+    #print(index_ref)
+
+
+
+
+
+#pt.init()
--- Fair-Ranking-LTR/main.py
+++ Fair-Ranking-LTR/main.py
@@ -1,22 +1,54 @@
 import pyterrier as pt
-from data import *
+from pyterrier.measures import *
+from data import download_data_p, download
 import os
+import pandas as pd
+import ir_datasets
 
-def main():
-    print('Heello')
+def main():  
+
+    print('Get data')
+    
+    dataset = pt.get_dataset('irds:trec-fair/2022')
+    train_dataset = pt.get_dataset('irds:trec-fair/2022/train')
+    # Index trec-fair/2022
+    indexer = pt.IterDictIndexer('./indices/trec-fair_2022', meta={'docno':25})
+    if not os.path.exists('./indices/trec-fair_2022'):
+        index = indexer.index(dataset.get_corpus_iter(), fields=['title', 'text'])
+    else:
+        index =pt.IndexFactory.of('./indices/trec-fair_2022')
+    print(index.getCollectionStatistics().toString())
+
+    topics = train_dataset.get_topics('text')
+    qrels = train_dataset.get_qrels()
+    print('Printing qrels: \n')
+    print("QID 84:",len(qrels[qrels['qid']=="84"]))
+    print("QID 100:",len(qrels[qrels['qid']=="100"]))
+
+
+    pipeline = pt.FeaturesBatchRetrieve(index, wmodel='BM25', features=["WMODEL:Tf","WMODEL:PL2"])
+    res = pipeline(topics)
+    print(res)
+    pt.Experiment(
+        [pipeline],
+        train_dataset.get_topics('text'),
+        train_dataset.get_qrels(),
+        [MAP, nDCG@20]
+    )
+
+
+    print('Process Successful')
+    return
 
 
 if __name__ == "__main__":
     os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64/"
-    
+    if pt.is_windows():
+        print('Operating System not Supported')
+    else:
+        print('Operating system supported!')
     pt.init()
     print('Pyterrier started')
-    downloaded_data = False
-    if downloaded_data == False:
-        
-        #download_data_p()
-        download_data('irds:trec-fair/2022')
-        
     main()
 
 
